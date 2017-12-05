3bcc6780e0f88a1e5c2641d049fab306659c7bcc
paulawaster <pbukowska16@gmail.com>
2017-12-05 22:13:24 +0100

    8.2 - zadanie poprawione (package test)

--- src/main/java/com/kodilla/exception/test/CallDivide.java
+++ src/main/java/com/kodilla/exception/test/CallDivide.java
@@ -10,9 +10,14 @@ public class CallDivide {
 
         FirstChallenge firstChallenge = new FirstChallenge();
 
-        double result = firstChallenge.divide(3, 1);
-
-        System.out.println(result);
+        try {
+            double result = firstChallenge.divide(3, 0);
+            System.out.println(result);
+        } catch (ArithmeticException e) {
+            System.out.println("Cannot divide by 0\n" + e);
+        } finally {
+            System.out.println("...Division attempt done...");
+        }
 
     }
 }
